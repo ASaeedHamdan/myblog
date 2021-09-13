6dd1569ae6aead5d59e26ad9753af0f362401c31
ASaeedHamdan <89150047+ASaeedHamdan@users.noreply.github.com>
2021-09-13 15:26:52 +0200

    Update log.tsx

--- pages/log.tsx
+++ pages/log.tsx
@@ -7,7 +7,7 @@ function Login()
          <form>
             <div><input type="email" placeholder="email here"></input></div> 
             <div><input type="password" placeholder="password here"></input></div> 
-            <button type="button" onClick={() => Router.push('/')} >login</button>
+            <button type="button" onClick={() => Router.push('/')} >login here</button>
          </form>
 
         </div>
